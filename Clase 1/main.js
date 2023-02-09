27243df4af81be790e21d9f08e81b594a15c90cc
zuelenrovira <zuelen.grovira@gmail.com>
2023-02-08 19:48:47 -0600

    clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,64 @@
+//comentario de linea
+/*   comentario
+de bloque*/  
+
+
+
+// Decalacion y asignación de una variable
+// let edad = 29;
+// let nombre = "zuelen"; // Forma recomendada
+// var nombre = "giselle"; //Forma no recomendADA
+// const NUMERO = 10;
+
+//let nombre ="camila";
+//console.log("aca esta el nombre", nombre);
+
+// nombreBonitoGrande // Camel case
+// nombre_bonito_grande //snake case
+// NombreBonitoGrande // pascal case
+
+//let numeroUno = 4;
+//let numeroDos = 5;
+
+//let resultadoSuma = numeroUno + numeroDos;
+//let resultadoResta = numeroUno - numeroDos;
+//let resultadoMultiplicacion = numeroUno * numeroDos;
+//let resultadoDivicion = numeroUno / numeroDos;
+
+// console.log("resultado de la suma", resultadoSuma);
+// console.log("resultado de la resta", resultadoResta);
+// console.log("resultado de la multiplicacion", resultadoMultiplicacion);
+// console.log("resultado de la division", resultadoDivicion);
+
+// let saludo = "hola";
+// let nombre = "Nata";
+
+//let mensaje = saludo + " " + nombre;
+
+// let mensaje = `esto es el saludo ${saludo} ${nombre}`;
+// console.log(mensaje);
+// console.log("hola" + " " + 1);
+// console.log(4 + 1);
+
+//console.log("hola");
+
+
+
+//let nombre = prompt("Ingrese su nombre");
+//alert(nombre);
+
+// let entrada = prompt("Ingresar una letra");
+// let salida = entrada + " " + "ingresada";
+// alert(salida);
+
+//parseInt() , parseFloat() , o Number()
+
+let edad = parseInt(prompt("¡Hola!, por favor ingrese su edad"));
+let sumaEdad = edad + 2;
+alert (`el resultado es ${sumaEdad}`);
+console.log(sumaEdad);
+
+
+
+
+
